ce3eacc7903e46992f503b9034c6b92c016656ae
blackhoundlabs <black.hound.labs@gmail.com>
2024-07-02 17:05:49 +0200

    chang viteconfig according https://vitejs.dev/guide/static-deploy.html#github-pages

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,8 @@ import react from '@vitejs/plugin-react'
 import basicSsl from '@vitejs/plugin-basic-ssl'
 
 // https://vitejs.dev/config/
-export default defineConfig({ 
+export default defineConfig({
+  base: '/cursoReact/',
   plugins: [react(), basicSsl()],
     build: {
       outDir: './docs'
